397b5ce4dafb76690ff43a393894ae4cb5e3b16d
David Muradyan <i22.muradyan.david@etud.ufar.am>
2025-01-15 23:53:59 +0400

    last-of-sprint1

--- Assets/MainGame/Scripts/GameManager.cs
+++ Assets/MainGame/Scripts/GameManager.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -6,6 +6,8 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    GameData Data;
+
     [Header("Fields")]
     [SerializeField] TextMeshProUGUI prof;
     [SerializeField] TextMeshProUGUI bio;
@@ -13,7 +15,6 @@ public class GameManager : MonoBehaviour
     [SerializeField] TextMeshProUGUI hobby;
     [SerializeField] TextMeshProUGUI phobia;
     [SerializeField] TextMeshProUGUI pers;
-    [SerializeField] TextMeshProUGUI edu;
     [SerializeField] TextMeshProUGUI add;
     [SerializeField] TextMeshProUGUI luggage;
 
@@ -21,12 +22,15 @@ public class GameManager : MonoBehaviour
         GameData Data = GameObject.Find("Data").GetComponent<DataLoader>().gameData;
 
         prof.text = Data.professions[Random.Range(0, Data.professions.Count)];
-        bio.text = Data.biology[Random.Range(0, Data.biology.Count)];
+
+        int age = Random.Range(18, 90);
+        bio.text = age + " տարեկան " + Data.biology[Random.Range(0, Data.biology.Count)];
+
+
         health.text = Data.health[Random.Range(0, Data.health.Count)];
         hobby.text = Data.hobby[Random.Range(0, Data.hobby.Count)];
         phobia.text = Data.phobia[Random.Range(0, Data.phobia.Count)];
         pers.text = Data.personality[Random.Range(0, Data.personality.Count)];
-        edu.text = Data.education[Random.Range(0, Data.education.Count)];
         add.text = Data.additional_information[Random.Range(0, Data.additional_information.Count)];
         luggage.text = Data.luggage[Random.Range(0, Data.luggage.Count)];
     }
